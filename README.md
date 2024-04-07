b64e09e58830df62eecdae7bca0cef5102306f35
Gina Sciacchitano <gbell76767@gmail.com>
2024-04-07 12:13:04 -0400

    getter bug fix, package cleanup, basic readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Social-Network-API
+
+## Description
+This is a backend application that allows you to post, get, update, and delete users, their friends, their thoughts, and their reactions.
+
+## Instillation
+npm install
+
+## Usage
+To run, type node server into the terminal. To use the models and routes, you must use a 3rd party application like insomnia since this has no front end. This application uses localhost port 3001. You can post users, thoughts, and reactions, get all users or thoughts, or get users or thoughts by id. You can also update users and thoughts and delete users, thoughts, and reactions. Lastly, you can add or remove users from a user's friends list.
+
+## Credits
+N/A
+
+## Video Link
--- models/thought-model.js
+++ models/thought-model.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose')
-const dayjs = require('dayjs')
 
 const reactionSchema = mongoose.Schema({
     reactionId: {type: mongoose.ObjectId, default: new mongoose.Types.ObjectId},
     reactionBody: {type: String, required: true, maxLength: 280},
     username: {type: String, required: true},
     createdAt: {type: Date, default: Date.now(), get: (date) => {
-        return dayjs(date, 'MMM DD YYYY hh:mm')
+        return date.toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"}) + ' ' + date.toLocaleTimeString('en-us', {hour12: true})
     }}
-})
+}, {toJSON: {getters: true}})
 
 const thoughtSchema = mongoose.Schema({
     thoughtText: {type: String, required: true, maxLength: 280, minLength: 1},
     createdAt: {type: Date, default: Date.now(), get: (date) => {
-        return dayjs(date, 'MMM DD YYYY hh:mm')
+        return date.toLocaleDateString('en-us', { year:"numeric", month:"short", day:"numeric"})+ ' ' + date.toLocaleTimeString('en-us', {hour12: true})
     }},
     username: {type: String, required: true},
     reactions: [reactionSchema]
-}, {toJSON: {virtuals: true}})
+}, {toJSON: {virtuals: true, getters: true}})
 
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length
